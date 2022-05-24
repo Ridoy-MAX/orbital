d6af61c335953c12417d9d09242c998dfee879c4
Ridoy-MAX <pou4w98y@gmail.com>
2022-05-25 01:36:04 +0600

    chang-1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
 
+function linkAction(){
+ 
 
 
 
+
+    $('.banner-slider1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots:false,
+        arrows:false,
+        autoplaySpeed: 2000,
+        // nextArrow:'<i class="fas fa-chevron-left  next-arrow"></i>',
+        // prevArrow:'<i class="fas fa-chevron-right  previous-arrow"></i>',
+       
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+          
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+         
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+    
+
+
+
+
+
+
+
+
+    }
 
 
 
